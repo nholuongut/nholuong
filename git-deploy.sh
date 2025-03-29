19ca70354783894ef74093cf84e6f02d5f13ca43
Nho Luong <nholu@nholuongs-MBP.lan>
2025-03-29 10:10:41 +0700

    Add cron for daily update

--- git-deploy.sh
+++ git-deploy.sh
@@ -2,7 +2,7 @@
 
 echo "Add files and do local commit"
 git add .
-git commit -am "Welcome to my repositories DevOps blogs"
+git commit -am "Add cron for daily update"
 
 echo "Pushing to my github repository"
 git push origin main --force
